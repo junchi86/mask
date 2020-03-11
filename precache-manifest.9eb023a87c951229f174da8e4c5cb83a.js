735b7921dc8cf13940b510977a850f5411d7330e
joon <56216292+junchi86@users.noreply.github.com>
2020-03-12 02:38:29 +0900

    수정

---
+++ precache-manifest.9eb023a87c951229f174da8e4c5cb83a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f0fa0592a10e035e901a80f4545fe70",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5325e1b756264b03f978",
+    "url": "./static/css/main.20faa3da.chunk.css"
+  },
+  {
+    "revision": "43433afdf6cf336178a6",
+    "url": "./static/js/2.8bafd61b.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "./static/js/2.8bafd61b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5325e1b756264b03f978",
+    "url": "./static/js/main.03c1de87.chunk.js"
+  },
+  {
+    "revision": "924f1ed2fcb4d5f13259",
+    "url": "./static/js/runtime-main.4f8e16d7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c62c863759148854f0732d734b9fc2d8.js"
+  "./precache-manifest.9eb023a87c951229f174da8e4c5cb83a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
