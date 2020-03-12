5918b77165cae6e3f5d96dc219671f64b7d554e4
joon <56216292+junchi86@users.noreply.github.com>
2020-03-13 00:06:49 +0900

    첫 로딩 시 위치 이슈 수정

--- precache-manifest.1c4ee6df2436ef17ca437ffd10d716dc.js
+++ precache-manifest.1c4ee6df2436ef17ca437ffd10d716dc.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9bd049bb0a48fc5787e3c09cafb7ffd6",
+    "revision": "cf03e1703fb77cc56da3cdf4f7a2ff80",
     "url": "./index.html"
   },
   {
-    "revision": "e50c9206164a020f3ea9",
+    "revision": "a6cad2e128aaabfb51b3",
     "url": "./static/css/main.c19ac76e.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.3758a475.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e50c9206164a020f3ea9",
-    "url": "./static/js/main.30fe42b2.chunk.js"
+    "revision": "a6cad2e128aaabfb51b3",
+    "url": "./static/js/main.930c16db.chunk.js"
   },
   {
     "revision": "924f1ed2fcb4d5f13259",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.8f244e0b85c3395e11dad0d689503826.js"
+  "./precache-manifest.1c4ee6df2436ef17ca437ffd10d716dc.js"
 );
 
 self.addEventListener('message', (event) => {
